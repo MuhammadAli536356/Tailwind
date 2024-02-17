5a87ff073962978b120603b1a123cb4b215aa0e5
MuhammadAli536356 <mabeer536356@gmail.com>
2024-02-18 00:05:28 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  // * means to write css for all pages fr one particular page you write name of html file inplace of *
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
